60ca5931fc7e9ca2abe91b474628b06c9293d89c
David Fuentes <ing.davidfuentes@outlook.com>
2017-12-29 12:45:49 -0300

    Limpiesa de Tickets Gestion de Reclamos

--- Asociar_Centro-Costo_Produccion.sql
+++ Asociar_Centro-Costo_Produccion.sql
@@ -1,5 +1,5 @@
 /*
-seguros vida security 
+seguros vida security
 07-018
 
 
@@ -17,16 +17,16 @@ ID = 216
 *	Obtener ID de la Empresa
 */
 select name, id from core.companies
-where name like '%ingenieria%';
+where name like '%mauricio%';
 
-ID = 1516
+ID = 586
 
 
 /*
 *	Obtenemos el ID del Centro de Costo
 */
 Select * from core.cost_centers
-where code = '07-018'
+where code = '07-017'
 and status = 1
 and deleted_at is null;
 
@@ -34,22 +34,23 @@ Select * from core.cost_centers
 where name like '%Servicio%';
 
 07-014 - GITBS - Venta de Equipos.  ==> 435
+07-017 - GITBS - Ventas Servicios UPS   ==> 308
 
-/* 
+/*
 *	Insertar registro en la Base de Datos con los Valores Obtenidos
 */
 
 
 INSERT INTO `core`.`companies_cost_centers`
-(`id`, `created_at`, `updated_at`, `author_id`, `updater_id`, `company_id`, `cost_center_id`, `deleted_at`) 
+(`id`, `created_at`, `updated_at`, `author_id`, `updater_id`, `company_id`, `cost_center_id`, `deleted_at`)
 VALUES
 (null,
 now(),
 now(),
 216,      -- Mi Usuario ID
 null,
-1572,     -- Company ID
-309,      -- Centro de Costo ID
+586,     -- Company ID
+308,      -- Centro de Costo ID
 null);
 
 -- Delete Centro de Costo
@@ -74,19 +75,19 @@ from core.companies_cost_centers cost
 		cost.company_id = comp.id
 	INNER JOIN core.cost_centers cc ON
 		cost.cost_center_id = cc.id
-	
+
 	order by cost.id DESC limit 1;
-    
-    
-    
+
+
+
     select * from `core`.`companies_cost_centers` where company_id = 660;
-    
+
     select * from core.companies where name like '%Mannk%';
-    
+
     select * from core.companies where id = 7;
-    
+
     select * from core.companies_relations;
-    
+
     select * from core.cost_centers;
 
 
@@ -99,38 +100,38 @@ from core.companies_cost_centers cost
 	LEFT JOIN core.cost_centers cc ON
 		cost.cost_center_id = cc.id
 	where comp.id = 7
-    
+
 	order by cost.id DESC limit 1;
-    
-    
-    
-    
+
+
+
+
     select * from core.cost_centers_departments;
-    
+
     select * from security_users where name like '%natalia%';
-    
-    select * from core.departments where id = 38;  -- Recursos Humanos 
-    
+
+    select * from core.departments where id = 38;  -- Recursos Humanos
+
     select * from core.cost_centers_departments dep
     JOIN core.cost_centers cc ON dep.cost_center_id = cc.id
     where dep.department_id = 38;
-    
-    
+
+
     select distinct(frecuency_type)  from blueisocial.kpi_customer_satisfaction
-    
+
     -- Monthly
     -- Bimonthly
     -- Quarterly
     -- Biannual
     -- Annual
-    
-    
+
+
     -- mensual, bimensual, trimestral, cuatrimestral, semestral y anual
-    
+
     -- 309 07-018
-    select * from core.companies_cost_centers where company_id = 1528;  
-    
-    
-    
-    
-    
+    select * from core.companies_cost_centers where company_id = 1528;
+
+
+
+
+
